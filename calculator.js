e6fe0eb2edbcff57ed390892442db884b28059c3
Surry Mowery <surrykj@gmail.com>
2023-03-09 15:53:01 -0800

    all buttons working

---
+++ calculator.js
@@ -0,0 +1,95 @@
+let total = 0;
+let screenNum = "0";
+let previousChar;
+let screen = document.querySelector(".screen");
+
+function buttonClick(input) {
+  if (isNaN(parseInt(input))) {
+    handleSymbol(input);
+  } else {
+    handleNumber(input);
+  }
+  renderOnScreen()
+  // console.log(input)
+}
+
+function handleSymbol(input) {
+  switch (input) {
+    case "C":
+      screenNum = "0";
+      total = 0;
+      break;
+    case "←":
+      // console.log(`screenNum: ${screenNum}`)
+      if (screenNum === "0" || screenNum.length === 1 || typeof(screenNum) != "string") {
+        screenNum = "0";
+      } else {
+        let editedNum = screenNum.slice(0, -1);
+        screenNum = editedNum;
+      }
+      break;
+    case "=":
+      completeEquation(parseInt(screenNum))
+      previousChar = null;
+      break;
+    case "÷":
+    case "×":
+    case "-":
+    case "+":
+      handleMath(input);
+      break;
+  };
+}
+
+function handleNumber(input) {
+  if (screenNum === "0") {
+    screenNum = input;
+  } else {
+    screenNum += input;
+  }
+}
+
+function handleMath(input) {
+  if (screenNum === "0") {
+    return;
+  }
+  let intInput = parseInt(screenNum)
+  if (total === 0) {
+    total = intInput;
+  } else {
+    completeEquation(intInput);
+  }
+  previousChar = input;
+  screenNum = "0";
+}
+
+function completeEquation(intInput) {
+  if (previousChar === "+") {
+    total += intInput;
+  } else if (previousChar === "-") {
+    total -= intInput
+  } else if (previousChar === "×") {
+    total *= intInput;
+  } else if (previousChar === "÷") {
+    total /= intInput;
+  }
+  screenNum = total
+  // console.log(`previousChar: ${previousChar}`)
+  // console.log(`total: ${total}`)
+  // console.log(`screenNum: ${screenNum}`)
+  // console.log(`intInput: ${intInput}`)
+}
+
+function renderOnScreen() {
+  screen.innerText = screenNum;
+}
+
+function init() {
+  document
+  .querySelector(".calculator-buttons")
+  .addEventListener("click", (e) => {
+    buttonClick(e.target.innerText);
+  })
+}
+
+init();
